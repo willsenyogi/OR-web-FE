0bdaa02c8c1deaca5d284d02250837b1822be94e
Willsen Yogi <willsenyp21@gmail.com>
2025-10-17 21:03:50 +0700

    sonner styling

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,6 +10,11 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      toastOptions={{
+        classNames: {
+          description: "toast-description",
+        },
+      }}
       style={
         {
           "--normal-bg": "var(--popover)",
